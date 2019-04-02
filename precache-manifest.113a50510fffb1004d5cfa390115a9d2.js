12f28137b555408fd752cfc77fdd7df669d09213
darius maximus <dariulqa@wp.pl>
2019-04-02 14:56:50 +0200

    Updates

---
+++ precache-manifest.113a50510fffb1004d5cfa390115a9d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1108e38baa8e4a90a2a2",
+    "url": "/Text-Booster/static/js/runtime~main.2e479c3e.js"
+  },
+  {
+    "revision": "69681b7a682cd1d5f67b",
+    "url": "/Text-Booster/static/js/main.2f427645.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/Text-Booster/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "69681b7a682cd1d5f67b",
+    "url": "/Text-Booster/static/css/main.f37883d8.chunk.css"
+  },
+  {
+    "revision": "0fb9f10d17cffe873fb36e56d77f0322",
+    "url": "/Text-Booster/index.html"
+  }
+];
